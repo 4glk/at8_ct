3bbfec890f8c9e2069406bb1250e1e3ec3075cc1
geraki <falcon2981@gmail.com>
2013-11-11 02:15:03 +0400

    dispatch update task good

--- sheduler/dispatch.c
+++ sheduler/dispatch.c
@@ -4,15 +4,29 @@
 void AddTask (void (*taskfunc)(void), uint16_t taskdelay){
    uint8_t n=0;
    uint8_t position=0;
-   // крутить пока : не попадется функция или пустая функция или пустой отсчет  и отсчет меньше предыдущих и не больше количества задач
+   // крутить пока : не попадется функция или пустая функция или пустой отсчет  и отсчет больше следующих и не больше количества задач
    while (((TaskArray[n].pfunc!=(*taskfunc))&&((TaskArray[n].pfunc!=0)||(TaskArray[n].countdown!=0)))&&(TaskArray[n].countdown<=((taskdelay==0)?(++taskdelay):(taskdelay))&&(n < MAXnTASKS)))n++;
     position=n;
         if ((TaskArray[position].pfunc==(*taskfunc))){// если есть уже такая функция в списке , то добавляем ей время
-            TaskArray[position].countdown += taskdelay;
+             TaskArray[position].countdown += taskdelay;  // прибавили время , нужно еще раз сортировать вниз по очереди
+                                                        // как бы это сделать без goto и лишних функций
+                                                        // еще один проход ничего не изменит т.к он найдет эту же функцию
+                                                        // как вариант разбить на функции , но как с передачей имени функции ???
+                                                        // перемотаем до конца или пока счетчики меньше текущего
+        task tmp2;
+        while ((TaskArray[n+1].pfunc!=0)&&(TaskArray[n+1].countdown<=TaskArray[n].countdown)){
+                tmp2=TaskArray[n+1];
+                TaskArray[n+1]=TaskArray[n];
+                TaskArray[n]=tmp2;
+                n++;
+        }
+        //while (((TaskArray[n].pfunc!=0)||(TaskArray[n].countdown!=0))&&(TaskArray[n].countdown<=(TaskArray[position].countdown))&&(n < MAXnTASKS)))n++;
+        // а теперь нужно сдвинуть очередь :) теперь наверное нужна новая позиция , здесь скорее пойдет сортировка перестановкой
+        // чтож , еще одна временная структура
         }else {
-        while ((TaskArray[n].pfunc != 0) && (TaskArray[n].countdown!=0) && (n < MAXnTASKS))n++;
-        for (/*.*/;n>position;n--){TaskArray[n]=TaskArray[n-1];}
-        TaskArray[position].pfunc = *taskfunc;
+        while ((TaskArray[n].pfunc != 0) && (TaskArray[n].countdown!=0) && (n < MAXnTASKS))n++;   // этот цикл мотает до конца очереди
+        for (/*.*/;n>position;n--){TaskArray[n]=TaskArray[n-1];} // этот сдвигает с конца до позиции
+        TaskArray[position].pfunc = *taskfunc;                      // если присвоить раньше  , то значение потеряется??? да!
         TaskArray[position].countdown = taskdelay;
         }
        if (n==0){ delay_time=TaskArray[0].countdown;dt=delay_time;} //если в очереди пусто вроде
